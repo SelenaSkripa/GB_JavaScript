25dbe3e1c1a3fd8227c9ee81cb58292c5904bb07
SelenaSkripa <SuhanovaElena.ru@yandex.ru>
2021-04-14 19:09:24 +0300

    Добавка к заданию

--- chess.js
+++ chess.js
@@ -7,8 +7,6 @@ function initBoard() {
 
 }
 
-window.onload = initBoard;
-
 for (var n = 0; n != 110; n++) {
     window.onload = initBoard();
 }
@@ -106,7 +104,38 @@ bgColor[92].innerHTML = "<img src='img/wq.png'>";
 bgColor[93].innerHTML = "<img src='img/wk.png'>";
 
 
+//шахматы учителя
 
+function chessboard() {
+    function addRow(board, rowNum, figures) {
+        var row = board.insertRow(-1);
+        row.insertCell(-1).innerText = rowNum;
+        for (var fig of figures) {
+            row.insertCell(-1).innerText = fig;
+        }
+        row.insertCell(-1).innerText = rowNum;
+    }
+    var board = document.createElement('table');
+    addRow(board, '', ['A','B','C','D','E','F','G','H']);
+    addRow(board, '1', ['♖','♘','♗','♕','♔','♗','♘','♖']);
+    addRow(board, '2', ['♙','♙','♙','♙','♙','♙','♙','♙']);
+    for (var i=3; i<=6; i++) {
+        addRow(board, i, '        ');
+    }
+    addRow(board, '7', ['♟','♟','♟','♟','♟','♟','♟','♟']);
+    addRow(board, '8', ['♜','♞','♝','♚','♛','♝','♞','♜']);
+    addRow(board, '', ['A','B','C','D','E','F','G','H']);
+    for (var i=1; i<=8; i++) {
+        for (var j=1; j<=8; j++) {
+            if (i % 2 + j % 2 === 1) {
+                board.rows[i].cells[j].style.backgroundColor = 'grey';
+            }
+        }
+    }
+    document.body.appendChild(board);
+};
+
+chessboard();
 
  
 
